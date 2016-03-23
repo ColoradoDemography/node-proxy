ac9478e0aeb8c3e64c76c0f5cd5ecaccb11eadf9
Daniel Trone <royhobbstn@yahoo.com>
2016-03-23 22:10:10 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var http = require('http'),
+    httpProxy = require('http-proxy');
+//
+// Create your proxy server and set the target in the options.
+//
+httpProxy.createProxyServer({target:'http://104.154.66.108:4003'}).listen(4001); // See (†)
+
+//
+// Create your target server
+//
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('request successfully proxied!' + '\n' + JSON.stringify(req.headers, true, 2));
+  res.end();
+}).listen(4003);
