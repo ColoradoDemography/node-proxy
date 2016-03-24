00a18709d5c88ce6c652b62604da6d26f396f99f
Daniel Trone <royhobbstn@yahoo.com>
2016-03-24 15:55:35 +0000

    catch errors

--- index.js
+++ index.js
@@ -8,5 +8,13 @@ var http = require('http'),
 // change 80 to 443 when SSL installed
 // new name of image might be codemog/node-proxy
 
-httpProxy.createProxyServer({target:'http://127.0.0.1:4001'}).listen(3000); // See (†)
+httpProxy.createProxyServer({target:'http://127.0.0.1:4001',localAddress:'127.0.0.1'}).listen(3000); // See (†)
 
+// Listen for the `error` event on `proxy`.
+httpProxy.on('error', function (err, req, res) {
+  res.writeHead(500, {
+    'Content-Type': 'text/plain'
+  });
+
+  res.end('Something went wrong. And we are reporting a custom error message.');
+});
