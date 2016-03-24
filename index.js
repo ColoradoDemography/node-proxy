f0a7206d0a6cd5bb33d3063f43f2988de119ca37
Daniel Trone <royhobbstn@yahoo.com>
2016-03-24 16:07:16 +0000

    format hostname

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ var http = require('http'),
 // change 80 to 443 when SSL installed
 // new name of image might be codemog/node-proxy
 
-httpProxy.createProxyServer({target:'lookups:4001'}).listen(3000); // See (†)
+httpProxy.createProxyServer({target:'http://lookups:4001'}).listen(3000); // See (†)
 
