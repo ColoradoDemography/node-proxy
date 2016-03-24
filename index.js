e0d833ace157c9ebb75a166d53952bb92010fede
Daniel Trone <royhobbstn@yahoo.com>
2016-03-24 16:14:05 +0000

    working - learned to link containers

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ var http = require('http'),
 // Create your proxy server and set the target in the options.
 //
 
-// docker run --name nodeproxy -p 80:3000 -d royhobbstn/node-proxy
+// docker run --name nodeproxy --link lookups:lookups -p 80:3000 -d codemog/node-proxy
 // change 80 to 443 when SSL installed
-// new name of image might be codemog/node-proxy
 
 httpProxy.createProxyServer({target:'http://lookups:4001'}).listen(3000); // See (†)
-
+//httpProxy.createProxyServer({target:'http://lookups:4001'}).listen(3000); // See (†)
