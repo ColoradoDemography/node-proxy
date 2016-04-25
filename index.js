1bb7282b4d355adbea7d156c960e83615e3e6cdc
Daniel Trone <royhobbstn@users.noreply.github.com>
2016-04-25 12:43:42 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-var http = require('http'),
-    httpProxy = require('http-proxy');
-//
-// Create your proxy server and set the target in the options.
-//
 
 // docker run --name nodeproxy --link lookups:lookups --link shinyserver:shinyserver -p 80:3000 -d codemog/node-proxy
 // change 80 to 443 when SSL installed
 
-
-httpProxy.createProxyServer({target:'http://lookups:4001'}).listen(3000); // See (†)
-httpProxy.createProxyServer({target:'http://shinyserver:3838'}).listen(3000); // See (†)
+var http = require('http');
+var httpProxy = require('http-proxy');
+var httpProxy.createServer({
+  hostnameOnly: true,
+  router: {
+    '/lookups': 'lookups:4001',
+    '/shiny' : 'shinyserver:3838'
+  }
+}).listen(3000);
