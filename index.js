4d6a14ee2191f5600ca9f4f7654c1bf9f9e92f1f
Daniel Trone <royhobbstn@users.noreply.github.com>
2016-04-25 10:33:09 -0600

    add support for shiny

--- index.js
+++ index.js
@@ -4,8 +4,19 @@ var http = require('http'),
 // Create your proxy server and set the target in the options.
 //
 
-// docker run --name nodeproxy --link lookups:lookups -p 80:3000 -d codemog/node-proxy
+// docker run --name nodeproxy --link lookups:lookups --link shinyserver:shinyserver -p 80:3000 -d codemog/node-proxy
 // change 80 to 443 when SSL installed
 
-httpProxy.createProxyServer({target:'http://lookups:4001'}).listen(3000); // See (†)
+var options = {
+  pathnameOnly: true,
+  router: {
+    '/lookups': 'lookups:4001',
+    '/shiny': 'shinyserver:3838'
+  }
+};
+
+// bind to port 80 on the specified IP address
+httpProxy.createServer(options).listen(3000);
+
 //httpProxy.createProxyServer({target:'http://lookups:4001'}).listen(3000); // See (†)
+//httpProxy.createProxyServer({target:'http://shinyserver:3838'}).listen(3000); // See (†)
