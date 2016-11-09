d2352478527a0ba70d54bbe45ebdd7599facbae4
Rob Kemp <rjameskemp@gmail.com>
2016-11-09 11:08:11 -0700

    updated ssl locations

--- index.js
+++ index.js
@@ -35,9 +35,9 @@ http.createServer(http_app).listen(HTTP_PORT).on('listening', function() {
 
 var sslobj={
         port: 443,
-        key: 'ssl/docker/gis_dola_colorado_gov.key',
-        cert: 'ssl/docker/ServerCertificate.crt',
-        ca: 'ssl/docker/ChainBundle2.crt'
+        key: 'ssl/docker/privkey1.pem',
+        cert: 'ssl/docker/fullchain1.pem',  
+        ca: 'ssl/docker/chain1.pem''
 };
 
 var redbird = require('redbird')({
