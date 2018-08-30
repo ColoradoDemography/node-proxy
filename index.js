dd7ee8d1b7e6b7105dbb472b9b6374fc81eb433b
TBleess <todd.bleess@gmail.com>
2018-08-30 09:50:56 -0600

    Going to try moving shinyproxy to its own r-proxy

--- index.js
+++ index.js
@@ -57,4 +57,4 @@ redbird.register('gis.dola.colorado.gov/sd', 'http://sdapi:4006', {ssl: true});
 redbird.register('gis.dola.colorado.gov/phantom', 'http://phantom:4007', {ssl: true});
 redbird.register('gis.dola.colorado.gov/usda_webmap', 'http://usda_webmap:4009', {ssl: true});
 redbird.register('gis.dola.colorado.gov/munis', 'http://muniapi:4010', {ssl: true});
-redbird.register('gis.dola.colorado.gov/profiles', 'http://shinyproxy:8080', {ssl: true});
+//redbird.register('gis.dola.colorado.gov/profiles', 'http://shinyproxy:8080', {ssl: true});
