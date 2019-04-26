94bb8d1e26142dc93e4053dc1e04a165914af5e9
TBleess <todd.bleess@gmail.com>
2019-04-26 13:28:22 -0600

    Update index.js

--- index.js
+++ index.js
@@ -56,5 +56,5 @@ redbird.register('gis.dola.colorado.gov/sd', 'http://sdapi:4006', {ssl: true});
 redbird.register('gis.dola.colorado.gov/phantom', 'http://phantom:4007', {ssl: true});
 redbird.register('gis.dola.colorado.gov/usda_webmap', 'http://usda_webmap:4009', {ssl: true});
 redbird.register('gis.dola.colorado.gov/munis', 'http://muniapi:4010', {ssl: true});
-redbird.register('gis.dola.colorado.gov/theia', 'http://theia:4011', {ssl: true});
+redbird.register('gis.dola.colorado.gov/theia', 'http://theia:3000', {ssl: true});
 
